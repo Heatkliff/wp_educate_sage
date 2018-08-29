4b7eeab1735f26fd978a33810fbb770fc9694f87
Heathkliff <bahyce@gmail.com>
2018-08-29 15:16:39 +0300

    #11465 Blog Page (Sage) - Home :: Create menu

--- wp-content/themes/sageeducation/resources/views/partials/header.blade.php
+++ wp-content/themes/sageeducation/resources/views/partials/header.blade.php
@@ -1,9 +1,12 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <a class="brand" href="{{ home_url('/') }}">
+      <img src="<?php echo get_site_icon_url() ?>">
+      <div class="blog_info">{{ get_bloginfo('name', 'display') }}</div>
+    </a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        {!!  wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
   </div>
