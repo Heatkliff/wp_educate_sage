5a3a594888c436fa5878bbff33f71456ca25b2fe
Heathkliff <bahyce@gmail.com>
2018-08-29 17:51:54 +0300

    #11465 Blog Page (Sage) - Home :: Create Slider

--- wp-content/themes/sageeducation/resources/views/partials/page-header.blade.php
+++ wp-content/themes/sageeducation/resources/views/partials/page-header.blade.php
@@ -1,3 +1,41 @@
 <div class="page-header">
+  <div class="gallery autoplay items-3">
+    <div id="item-1" class="control-operator"></div>
+    <div id="item-2" class="control-operator"></div>
+    <div id="item-3" class="control-operator"></div>
+
+    <figure class="item">
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <h1>Item 1</h1>
+    </figure>
+
+    <figure class="item">
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <h1>Item 2</h1>
+    </figure>
+
+    <figure class="item">
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <h1>Item 3</h1>
+    </figure>
+
+    <div class="controls">
+      <a href="#item-1" class="control-button">•</a>
+      <a href="#item-2" class="control-button">•</a>
+      <a href="#item-3" class="control-button">•</a>
+    </div>
+  </div>
   <h1>{!! App::title() !!}</h1>
 </div>
