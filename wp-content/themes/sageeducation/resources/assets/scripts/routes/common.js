d2e8b18a244f4cdcfb078adf6d6201c69886f907
Heathkliff <bahyce@gmail.com>
2018-08-30 17:57:38 +0300

    #11465 Blog Page (Sage) - Home :: Update slider, do autoscroll

--- wp-content/themes/sageeducation/resources/assets/scripts/routes/common.js
+++ wp-content/themes/sageeducation/resources/assets/scripts/routes/common.js
@@ -23,4 +23,50 @@ jQuery(function ($) {
     $(this).fadeOut('fast');
     slideMenu.slideUp(300);
   });
+
+  $(document).ready(function() {
+    $(".slider").each(function () { // обрабатываем каждый слайдер
+      var obj = $(this);
+      $(obj).append("<div class='nav'></div>");
+      $(obj).find("li").each(function () {
+        $(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
+        $(this).addClass("slider"+$(this).index());
+      });
+      $(obj).find("span").first().addClass("on"); // делаем активным первый элемент меню
+    });
+  });
+  function sliderJS (obj, sl) { // slider function
+    var ul = $(sl).find("ul"); // находим блок
+    var bl = $(sl).find("li.slider"+obj); // находим любой из элементов блока
+    var step = $(bl).width(); // ширина объекта
+    $(ul).animate({marginLeft: "-"+step*obj}, 1000); // 500 это скорость перемотки
+  }
+  $(document).on("click", ".slider .nav span", function() { // slider click navigate
+    var sl = $(this).closest(".slider"); // находим, в каком блоке был клик
+    $(sl).find("span").removeClass("on"); // убираем активный элемент
+    $(this).addClass("on"); // делаем активным текущий
+    var obj = $(this).attr("rel"); // узнаем его номер
+    sliderJS(obj, sl); // слайдим
+    return false;
+  });
+
+
+  window.onload = function autoscroll() {
+    var checks = $(document).find('.slider .nav span');
+    var check_now = 1;
+
+    setInterval(function() {
+      console.log('Checks = ' + checks.length + ". Check now - " + check_now);
+      checks[check_now].click();
+
+      if (check_now+1 < checks.length){
+        check_now += 1;
+      }else {
+        check_now = 0;
+      }
+    }, 5000);
+
+  };
+  
 });
+
--- wp-content/themes/sageeducation/resources/views/partials/page-header.blade.php
+++ wp-content/themes/sageeducation/resources/views/partials/page-header.blade.php
@@ -1,41 +1,77 @@
 <div class="page-header">
-  <div class="gallery autoplay items-3">
-    <div id="item-1" class="control-operator"></div>
-    <div id="item-2" class="control-operator"></div>
-    <div id="item-3" class="control-operator"></div>
-
-    <figure class="item">
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <h1>Item 1</h1>
-    </figure>
-
-    <figure class="item">
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <h1>Item 2</h1>
-    </figure>
-
-    <figure class="item">
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <h1>Item 3</h1>
-    </figure>
-
-    <div class="controls">
-      <a href="#item-1" class="control-button">•</a>
-      <a href="#item-2" class="control-button">•</a>
-      <a href="#item-3" class="control-button">•</a>
+    <div class="slider">
+        <ul>
+            <li>
+                <div class="item"
+                     style="background-image: url('https://theecologist.org/sites/default/files/2018-06/nature-view.jpg')"></div>
+                <div class="night-slide"></div>
+                <div class="content-clider">
+                    <div class="block-content-slider">
+                        <h1>Lorem ipsum</h1>
+                        <h3>Mauris lacinia arcu vel dui suscipit</h3>
+                        <div class="button-slider">
+                            <a href="#">Test button</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="item"
+                     style="background-image: url('https://i1.wp.com/decideyouradventure.com/wp-content/uploads/2017/08/indian-kill-nature-preserve-creek.jpg?resize=700%2C290&ssl=1')"></div>
+                <div class="night-slide"></div>
+                <div class="content-clider">
+                    <div class="block-content-slider">
+                        <h1>Sed vel orci</h1>
+                        <h3>Interdum et malesuada fames</h3>
+                        <div class="button-slider">
+                            <a href="#">Test button</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="item"
+                     style="background-image: url('https://i0.wp.com/discover-yourself.com/wp-content/uploads/2014/04/Whitton-Pond.jpg?resize=700%2C290')"></div>
+                <div class="night-slide"></div>
+                <div class="content-clider">
+                    <div class="block-content-slider">
+                        <h1>Maecenas enim diam</h1>
+                        <h3>Cras sodales turpis a orci</h3>
+                        <div class="button-slider">
+                            <a href="#">Test button</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="item"
+                     style="background-image: url('http://www.bellerieve.com/wp-content/uploads/2013/04/image6-700x290.jpg')"></div>
+                <div class="night-slide"></div>
+                <div class="content-clider">
+                    <div class="block-content-slider">
+                        <h1>Quisque interdum</h1>
+                        <h3>Vestibulum viverra efficitur metus</h3>
+                        <div class="button-slider">
+                            <a href="#">Test button</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="item"
+                     style="background-image: url('https://i.pinimg.com/originals/ef/fc/73/effc7377df71fc54a89533412f46a14a.jpg')"></div>
+                <div class="night-slide"></div>
+                <div class="content-clider">
+                    <div class="block-content-slider">
+                        <h1>Sed aliquam nisi</h1>
+                        <h3>Mauris non lobortis felis</h3>
+                        <div class="button-slider">
+                            <a href="#">Test button</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
-  </div>
-  <h1>{!! App::title() !!}</h1>
+    <h1>{!! App::title() !!}</h1>
 </div>
