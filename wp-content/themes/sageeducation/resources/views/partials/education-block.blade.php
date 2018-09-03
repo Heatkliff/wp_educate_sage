789892320b7ce1d9041008f57ba140a9d215ad48
Heathkliff <bahyce@gmail.com>
2018-09-03 18:01:20 +0300

    #11465 Blog Page (Sage) - Home :: Education block ready for testing

---
+++ wp-content/themes/sageeducation/resources/views/partials/education-block.blade.php
@@ -0,0 +1,22 @@
+<div class="education-block">
+    {{--<pre>@php var_dump(get_field('educate_block','option')) @endphp</pre>--}}
+    @php $content_ed_block = get_field('educate_block','option') @endphp
+    <div class="image-block" style='background-image: url("{!! $content_ed_block["image_poster"]["url"] !!}")'></div>
+    <div class="educate-content">
+        <div class="title-educate-block">
+            {!! $content_ed_block["content_education_block"]["block_header"] !!}
+        </div>
+        <div class="content-educate-block">
+            {!! $content_ed_block["content_education_block"]["text_in_block"] !!}
+        </div>
+        @php $list = $content_ed_block["content_education_block"]["list_elements_in_block"] @endphp
+        <div class="educate-list">
+            @foreach($list as $item)
+                <div class="elem-list">
+                    <img src="{!! $item["image_in_list_block"]["url"] !!}" alt="">
+                    <div class="title-list">{!! $item["text_in_list_block"] !!}</div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
