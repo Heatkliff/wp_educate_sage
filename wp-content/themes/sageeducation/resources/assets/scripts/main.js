a3638cc41a055655ae3a1501a63fee4ad30a3da3
Heathkliff <bahyce@gmail.com>
2018-09-10 17:04:50 +0300

    #11465 Blog Page (Sage) - Home :: End work day work with slider courses

--- wp-content/themes/sageeducation/resources/assets/scripts/main.js
+++ wp-content/themes/sageeducation/resources/assets/scripts/main.js
@@ -9,6 +9,8 @@ import Router from './util/Router';
 import common from './routes/common';
 import home from './routes/home';
 import aboutUs from './routes/about';
+// import jqmobile from './routes/jquery.mobile-1.4.5.min';
+
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
@@ -18,6 +20,7 @@ const routes = new Router({
   home,
   // About Us page, note the change from about-us to aboutUs.
   aboutUs,
+  // Swipe for sliders
 });
 
 // Load Events
--- wp-content/themes/sageeducation/resources/views/partials/courses-block.blade.php
+++ wp-content/themes/sageeducation/resources/views/partials/courses-block.blade.php
@@ -8,27 +8,10 @@
             @php $course_fields = get_field('fields_cources_post',$course->ID,'option') @endphp
             @include('partials/courses-pre-post')
         @endforeach
-
     </div>
+    <div class="navigation"></div>
 
     <div class="load_all">
-        <pre>@php
-                $all_courses = new WP_Query(
-                array(
-                    'post_type' => 'courses',
-                ));
-                //var_dump($all_courses)
-            @endphp</pre>
-
-
-        @if (  $wp_query->max_num_pages > 1 )
-            <script>
-                var ajaxurl = '{!! site_url()  !!}/wp-admin/admin-ajax.php';
-                var true_posts = '{!! serialize($wp_query->query_vars) !!}';
-                var max_pages = '{!! $wp_query->max_num_pages  !!}';
-                console.log("ajaxurl - " + ajaxurl + " | true_posts - " + true_posts + " | max_pages" + max_pages)
-            </script>
-        @endif
         <button type="button" id="true_loadmore" class="btn btn-light">View All</button>
         <br>
     </div>
